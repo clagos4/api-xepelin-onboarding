6d9b1dabea471cbf93cacc2be2526778706aecba
Cristobal <clagos4@uc.cl>
2021-10-22 00:16:47 -0300

    feat: intermediate implementation of invoice getter

--- src/routes/api/v1/invoices.ts
+++ src/routes/api/v1/invoices.ts
@@ -4,19 +4,47 @@ import { PrismaClient } from '@prisma/client';
 const router = express.Router();
 const prisma = new PrismaClient();
 
-router.get('/:vendor?:invoice_date?:currency?', (req, res, next) => {
-    // const { vendor, invoice_date, currency } = req.params;
-    // var messageDetails = (vendor ? 'with vendor, ' : 'without vendor, ') + 
-    //     (invoiceDate ? 'with invoiceDate y ' : 'without invoiceDate y ') + 
-    //     (currency ? 'with currency, ' : 'without currency.');
-    
-    var messageDetails = ""
-
-    // if (!vendor) {messageDetails}
-
-    res.send({
-        message: 'Should return invoiceId, vendorId, invoiceNumber, invoiceTotal, paymentTotal, creditTotal y bankId.' + messageDetails
-    })
+router.get('/:vendor?/:invoiceDate?/:currency?', async (req, res, next) => {
+    const { vendor, invoiceDate, currency }: { [id: string] : String; } = req.params;
+    let params: any = {};
+    if (vendor) params.vendor_id = vendor;
+    if (vendor) params.invoice_date = invoiceDate;
+    console.log(params)
+    var invoices = await prisma.invoice.findMany({ 
+        select: {
+            invoice_id: true,
+            vendor_id: true,
+            invoice_number: true,
+            invoice_total: true,
+            payment_total: true,
+            credit_total: true,
+            bank_id: true
+        },
+        where: params 
+    });
+    if (currency) {
+        for (let index = 0; index < invoices.length; index++) {
+            const element = invoices[index];
+            await $.ajax({
+                url: process.env.CURRENCY_API_URL,
+                type: "get",
+                success: (data) => {
+                    invoices[index].payment_total = data;
+                }
+            })
+        }
+        invoices.forEach(async (invoice) => {
+            await $.ajax({
+                url: process.env.CURRENCY_API_URL,
+                type: "get",
+                success: (data) => {
+                    
+                }
+            })
+        })
+    }
+    res.status(200).send({message: JSON.stringify(invoices)});
 })
+
 
 export = router;
